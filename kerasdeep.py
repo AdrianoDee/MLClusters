8913687ecbd3f6c6d064c795169e862f42cf5ab8
AdrianoDee <adriano.diflorio@ba.infn.it>
2017-01-19 15:23:13 +0100

    Epochs for

--- kerasdeep.py
+++ kerasdeep.py
@@ -27,7 +27,7 @@ from io import StringIO
 
 size = 8
 pile = 2
-epoch = 1
+#epoch = 1
 batchsize = 1000
 lDel = False
 
@@ -63,119 +63,122 @@ clustercnn.compile(loss='binary_crossentropy',optimizer='sgd', metrics=['accurac
 
 results = []
 
-for batchsize in batchsizes:
-    print("-- Training")
-    history = History()
-    #early = ker.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.01, patience=0, verbose=0, mode='auto')
-    history = clustercnn.fit(X_train, y_train,validation_data=(X_test, y_test), nb_epoch=epoch, batch_size=batchsize,shuffle=True,verbose=0)#,callbacks=[early])
+for epoch in epochs:
+    for batchsize in batchsizes:
+        print("-- Training with %g (%g epochs)"%(batchsize,epoch))
+        history = History()
+        #early = ker.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.01, patience=0, verbose=0, mode='auto')
+        history = clustercnn.fit(X_train, y_train,validation_data=(X_test, y_test), nb_epoch=epoch, batch_size=batchsize,shuffle=True,verbose=0)#,callbacks=[early])
 
-    #%matplotlib inline
-    #show_losses([("mse",histo)],"Dense_test")
+        #%matplotlib inline
+        #show_losses([("mse",histo)],"Dense_test")
 
-    #print(history.history.keys())
+        #print(history.history.keys())
 
-    # summarize history for accuracy
-    predicted_target = clustercnn.predict(X_test)
+        # summarize history for accuracy
+        predicted_target = clustercnn.predict(X_test)
 
-    #print(predicted_target.shape())
-    #print(predicted_target)
-    #print(y_test)
-    print("-- Testing")
-    y_test_score = y_test[:,1]
-    predicted_target_score = predicted_target[:,1]
+        #print(predicted_target.shape())
+        #print(predicted_target)
+        #print(y_test)
 
-    truePosivites = []
-    falsePositive = []
-    values = []
+        y_test_score = y_test[:,1]
 
-    thresholds = np.linspace(np.amin(predicted_target_score), 1.0, num=10000)
+        falses = (y_test_score == 0.0).sum();
+        trues = (y_test_score == 1.0).sum();
 
-    #print (predicted_target_score)
-    #print (np.amin(predicted_target_score)*0.99999)
-    #print (thresholds)
+        print("-- Testing with dataset with %g false and %g true"%(falses,trues))
 
-    TruePositives = []
-    FalsePositives = []
+        predicted_target_score = predicted_target[:,1]
 
-    falses = (y_test_score == 0.0).sum();
-    trues = (y_test_score == 1.0).sum();
-    print(falses)
+        truePosivites = []
+        falsePositive = []
+        values = []
 
+        thresholds = np.linspace(np.amin(predicted_target_score), 1.0, num=1000)
 
-    for i in range(len(thresholds)):
-        low_values_indices_true = y_test_score >0.5
-        low_values_indices = predicted_target_score > thresholds[i]
-        #print(low_values_indices)
-        TruePositive = 0
-        FalsePositive = 0
-        TrueNegative = 0
-        FalseNegative = 0
-        All = len(low_values_indices)
+        #print (predicted_target_score)
+        #print (np.amin(predicted_target_score)*0.99999)
+        #print (thresholds)
 
-        for j in range(len(low_values_indices)):
-            if(low_values_indices_true[j]==False and low_values_indices[j]==False):
-                TrueNegative+=1
-            if(low_values_indices_true[j]==True and low_values_indices[j]==True):
-                TruePositive+=1
-    #                  trues +=1
-            if(low_values_indices_true[j]==True and low_values_indices[j]==False):
-                FalseNegative+=1
-    #                  trues += 1
-            if(low_values_indices_true[j]==False and low_values_indices[j]==True):
-                FalsePositive+=1
-    #                  falses +=1
+        TruePositives = []
+        FalsePositives = []
 
 
-        TruePositives.append(TruePositive/trues)
-        FalsePositives.append(FalsePositive/falses)
+        #print(falses)
 
-        truePosivites.append(TruePositive)
-        falsePositive.append(FalsePositive)
 
-        result = (epoch,batchsize,truePosivites,falsePositive)
+        for i in range(len(thresholds)):
+            low_values_indices_true = y_test_score >0.5
+            low_values_indices = predicted_target_score > thresholds[i]
+            #print(low_values_indices)
+            TruePositive = 0
+            FalsePositive = 0
+            TrueNegative = 0
+            FalseNegative = 0
+            All = len(low_values_indices)
+
+            for j in range(len(low_values_indices)):
+                if(low_values_indices_true[j]==False and low_values_indices[j]==False):
+                    TrueNegative+=1
+                if(low_values_indices_true[j]==True and low_values_indices[j]==True):
+                    TruePositive+=1
+        #                  trues +=1
+                if(low_values_indices_true[j]==True and low_values_indices[j]==False):
+                    FalseNegative+=1
+        #                  trues += 1
+                if(low_values_indices_true[j]==False and low_values_indices[j]==True):
+                    FalsePositive+=1
+        #                  falses +=1
+
+
+            TruePositives.append(TruePositive/trues)
+            FalsePositives.append(FalsePositive/falses)
+
+        result = (epoch,batchsize,TruePositives,FalsePositives)
         results.append(result);
 
-    #falsePositive = np.asarray(falsePositive,dtype=np.float32)
+        #falsePositive = np.asarray(falsePositive,dtype=np.float32)
 
 
-    plt.plot(FalsePositives,TruePositives)
-    plt.savefig("half_batch_%g_epoch_%g_trues.png"%(batchsize,epoch))
+        plt.plot(FalsePositives,TruePositives)
+        plt.savefig("half_batch_%g_epoch_%g_trues.png"%(batchsize,epoch))
 
-    TruePositives = np.asarray(TruePositives,dtype=np.float32)
-    FalsePositives = np.asarray(FalsePositives,dtype=np.float32)
+        TruePositives = np.asarray(TruePositives,dtype=np.float32)
+        FalsePositives = np.asarray(FalsePositives,dtype=np.float32)
 
-    np.savetxt("half_batch_%g_epoch_%g_trues.out"%(batchsize,epoch),(TruePositives))
-    np.savetxt("half_batch_%g_epoch_%g_falses.out"%(batchsize,epoch),(FalsePositives))
-    np.savetxt("half_batch_%g_epoch_%g_thresh.out"%(batchsize,epoch),(thresholds))
+        np.savetxt("half_batch_%g_epoch_%g_trues.out"%(batchsize,epoch),(TruePositives))
+        np.savetxt("half_batch_%g_epoch_%g_falses.out"%(batchsize,epoch),(FalsePositives))
+        np.savetxt("half_batch_%g_epoch_%g_thresh.out"%(batchsize,epoch),(thresholds))
 
-          #plt.savefig("trueFalse.png")
+              #plt.savefig("trueFalse.png")
 
-          #plt.figure(2)
-          #plt.plot(truePosivites,thresholds)
-          #plt.savefig("threshs.png")
+              #plt.figure(2)
+              #plt.plot(truePosivites,thresholds)
+              #plt.savefig("threshs.png")
 
-          #for i in range(len(histories)):
-          #      nn_graphics.show_losses(histories[i])
-          #      plt.plot(histories[i].history['acc'])
-          #      plt.plot(histories[i].history['val_acc'])
-          #      plt.title('model accuracy batchsize %d'%(batchsize))
-          #      plt.ylabel('accuracy')
-          #      plt.xlabel('epoch')
-          #      plt.legend(['train', 'test'], loc='upper left')
-          #      plt.savefig('myfig')
+              #for i in range(len(histories)):
+              #      nn_graphics.show_losses(histories[i])
+              #      plt.plot(histories[i].history['acc'])
+              #      plt.plot(histories[i].history['val_acc'])
+              #      plt.title('model accuracy batchsize %d'%(batchsize))
+              #      plt.ylabel('accuracy')
+              #      plt.xlabel('epoch')
+              #      plt.legend(['train', 'test'], loc='upper left')
+              #      plt.savefig('myfig')
 
-          #summarize history for loss
-          #print(clustercnn.summary())
+              #summarize history for loss
+              #print(clustercnn.summary())
 
-          #print("Threshold %g \n TruePositive %g \n FalsePositive %g \n TrueNegative %g \n FalseNegative %g"%(thresholds[i],TruePositive,FalsePositive,TrueNegative,FalseNegative))
+              #print("Threshold %g \n TruePositive %g \n FalsePositive %g \n TrueNegative %g \n FalseNegative %g"%(thresholds[i],TruePositive,FalsePositive,TrueNegative,FalseNegative))
 
-          #predicted_target_score_thr[low_values_indices] = 0.0
-          #predicted_target_score_thr[high_values_indices] = 1.0
-          #print("Threshold %g Ones %g zeros %g "%(thresholds[i],(predicted_target_score <= thresholds[i]).sum(),(predicted_target_score >= thresholds[i]).sum()))
-          #print(low_values_indices)
-          #print(high_values_indices)
-          #TruePositive, FalsePositive, TrueNegative, FalseNegative = ku.accuracy_measure(y_test_score, predicted_target_score_thr)
-          #value = [thresholds[i],TruePositive, FalsePositive, TrueNegative, FalseNegative]
-          #values.append(value)
-          #truePosivites.append(TruePositive)
-          #falsePositive.append(FalsePositive)
+              #predicted_target_score_thr[low_values_indices] = 0.0
+              #predicted_target_score_thr[high_values_indices] = 1.0
+              #print("Threshold %g Ones %g zeros %g "%(thresholds[i],(predicted_target_score <= thresholds[i]).sum(),(predicted_target_score >= thresholds[i]).sum()))
+              #print(low_values_indices)
+              #print(high_values_indices)
+              #TruePositive, FalsePositive, TrueNegative, FalseNegative = ku.accuracy_measure(y_test_score, predicted_target_score_thr)
+              #value = [thresholds[i],TruePositive, FalsePositive, TrueNegative, FalseNegative]
+              #values.append(value)
+              #truePosivites.append(TruePositive)
+              #falsePositive.append(FalsePositive)
