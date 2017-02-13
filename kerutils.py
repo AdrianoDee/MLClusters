a6cefedc9f2f25d9e7f3ff89d0bfbe85fddcc797
AdrianoDee <adriano.diflorio@ba.infn.it>
2017-02-13 13:20:31 +0100

    train_clusters

--- kerutils.py
+++ kerutils.py
@@ -344,9 +344,9 @@ def doubletsReadPostMod(detIn,detOu,modIn,modOu,datasets,train=False,
   truesIndex  =  (train_clusters_labels[:,1]==1.0)
   falsesIndex =  (train_clusters_labels[:,1]==0.0)
 
-  print(train_clusters)
+  #print(train_clusters)
 
-  trueClusters = numpy.array(numpy.extract(truesIndex, train_clusters))
+  trueClusters = numpy.extract(truesIndex, train_clusters)
   trueLabels   = numpy.extract(truesIndex, train_clusters_labels)
 
   falseClusters = numpy.extract(falsesIndex, train_clusters)
